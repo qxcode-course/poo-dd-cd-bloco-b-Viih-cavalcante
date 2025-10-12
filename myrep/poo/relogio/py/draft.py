626518cd2e2d2ec3b6109134dac479da07857a1c
Viih cavalcante <viih.cavalcante@alu.ufc.br>
2025-10-12 12:46:24 -0300

    tentando mais um pouquinho

--- myrep/poo/relogio/py/draft.py
+++ myrep/poo/relogio/py/draft.py
@@ -1,7 +1,13 @@
-class relogio :
+class Watch :
     def __init__(self) :
-        self._hora = int
-        self._minuto = int
-        self._segundos = int
+        self._hora :int = 0
+        self._minuto :int = 0
+        self._segundo :int = 0
 
-    def get
+    def mostrar(self) -> None:
+        print(self)
+
+
+    def __srt__(self):
+        return f"{self._hora:2d}:{self._minuto:2d}:{self._segundo}"
+def main():
