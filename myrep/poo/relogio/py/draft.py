454d6aa1f8eb4ac9037a1fdaade53a97c2367074
Viih cavalcante <viih.cavalcante@alu.ufc.br>
2025-10-13 23:04:55 -0300

    tentando mais um cadinho

--- myrep/poo/relogio/py/draft.py
+++ myrep/poo/relogio/py/draft.py
@@ -1,13 +1,26 @@
 class Watch :
-    def __init__(self) :
-        self._hora :int = 0
-        self._minuto :int = 0
-        self._segundo :int = 0
+    def __init__(self, hora: int, minuto: int, segundo: int):
+        self._hora :int = hora
+        self._minuto :int = minuto
+        self._segundo :int = segundo
 
     def mostrar(self) -> None:
         print(self)
 
 
-    def __srt__(self):
-        return f"{self._hora:2d}:{self._minuto:2d}:{self._segundo}"
+    def __str__(self)-> str:
+        return f"{self._hora:02d}:{self._minuto:02d}:{self._segundo:02d}"
 def main():
+    relogio = Watch (0, 0, 0)
+    while True:
+        line :str = input()
+        print("$" + line)
+        args : list [str] = line.split(" ")
+        cmd =  args[0]
+        if cmd == "end" :
+            break
+        elif cmd == "show":
+            relogio.mostrar()
+
+if __name__ == "__main__" :
+    main()
