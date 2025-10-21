62f75c9a1a9a42419b313bf4d19b519b1d493362
Viih cavalcante <viih.cavalcante@alu.ufc.br>
2025-10-21 03:13:51 -0300

    mi mir

---
+++ myrep/poo/camisa/py/draft.py
@@ -0,0 +1,7 @@
+class camisa:
+    def __init__(self) -> None :
+        self._Tamanho :str = ""
+    def getTamanho(self) -> str:
+        return  self._tamanho 
+    def setTamanho(self,valor: str):_tamanhos_validos = ["pp"
+    :::
