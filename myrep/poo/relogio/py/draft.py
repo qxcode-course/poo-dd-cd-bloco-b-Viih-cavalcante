7df6a29689888f812027e48574e948533720834e
Viih cavalcante <viih.cavalcante@alu.ufc.br>
2025-10-20 23:23:11 -0300

    consegui umaa

--- myrep/poo/relogio/py/draft.py
+++ myrep/poo/relogio/py/draft.py
@@ -9,7 +9,7 @@ class Watch :
 
 
     def __str__(self)-> str:
-        return f"{self._hora:02d}:{self._minuto:02d}:{self._minuto:02d}"
+        return f"{self._hora:02d}:{self._minuto:02d}:{self._segundo:02d}"
     def set(self, hora: int, minuto: int, segundo: int) -> None:
         if 0 <= hora <= 23:
             self._hora = hora
@@ -23,13 +23,30 @@ class Watch :
             self._segundo = segundo
         else:
             print("fail: segundo invalido")
-
+    def nextSecond(self) -> None :
+      self._segundo +=1
+      if  self._segundo == 60:
+        self._segundo = 0
+        self._minuto += 1
+      if self._minuto == 60:
+          self._minuto = 0
+          self._hora += 1
+      if self._hora == 24:
+          self._hora = 0
 def main():
     relogio = Watch (0, 0, 0)
 
     while True:
-        line :str = input()
-        print("$" + line)
+        try:
+            raw_line: str = input()
+        except EOFError:
+            break
+        print("$" + raw_line)
+
+        line = raw_line.strip()
+        if line == "":
+            continue
+
         args : list [str] = line.split()
         cmd =  args[0]
 
@@ -40,11 +57,26 @@ def main():
             relogio.mostrar()
 
         elif cmd == "set":
-            hora = int(args[1])
-            minuto = int(args[2])
-            segundo = int(args[3])
+            if len(args) < 4:
+                continue
+            try:
+             hora = int(args[1])
+             minuto = int(args[2])
+             segundo = int(args[3])
+            except ValueError :
+                continue
             relogio.set(hora, minuto, segundo)
-
+        elif cmd == "next":
+            relogio.nextSecond()
+        elif cmd == "init":
+            relogio = Watch(0, 0, 0)
+            if len(args) < 4:
+                continue
+            try:
+                h = int(args[1]); m = int(args[2]); s = int(args[3])
+            except ValueError:
+                continue
+            relogio.set(h, m, s)
 
 if __name__ == "__main__" :
     main()
