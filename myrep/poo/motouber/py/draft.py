d85f7995c1ab62a77f5fe567d3cccc8a3c9e045c
Viih cavalcante <viih.cavalcante@alu.ufc.br>
2025-10-28 14:20:15 -0300

    amém

--- myrep/poo/motouber/py/draft.py
+++ myrep/poo/motouber/py/draft.py
@@ -1,45 +1,70 @@
 class Pessoa:
     def __init__(self, name: str, money: int):
-        self._name: str = name
-        self._money: int = money
+        self.__name: str = name
+        self.__money: int = money
+
     def getName(self) -> str:
-        return self._name
+        return self.__name
     def getMoney(self) -> int:
-        return self._money
-    def setMoney(self, valor: int):
-        self._money = valor
-    def pagar(self, valor: int):
-        self._money -= valor
-    def receber(self, valor: int):
-        self._money += valor
+        return self.__money
+    def setMoney(self, valor: int) -> int:
+        self.__money = valor
+    def pagar(self, valor: int) -> int:
+        self.__money -= valor
+    def receber(self, valor: int) -> int:
+        self.__money += valor
     def __str__(self) -> str:
         return f"{self.getName()}:{self.getMoney()}"
 class Moto:
-    def __init__(self,custo :int):
-        self._custo: int = custo
-        self._motorista: Pessoa | None = None
-        self._passageiro: Pessoa | None =None
+    def __init__(self, custo:int):
+        self.__custo: int = custo
+        self.__motorista: Pessoa | None = None
+        self.__passageiro: Pessoa | None = None
     def getCusto(self) -> int:
-        return self._custo
+        return self.__custo
     def getDriver(self) -> Pessoa | None:
-        return self._motorista
-    def setDriver(self, motorista: Pesssoa):
-        self._motorista = motorista
-    def getPass(self,passageiro: Pessoa):
-        self._motoristrta = motorista
+        return self.__motorista
+    def setDriver(self, motorista: Pessoa):
+        self.__motorista = motorista
+    def getPass(self) -> Pessoa | None:
+        return self.__passageiro
     def setPass(self, passageiro: Pessoa):
-        self._passageiro = passageiro
+        self.__passageiro = passageiro
     def subir(self, passageiro: Pessoa):
-        if self._motorista == None:
+        if self.__motorista == None:
+            print("fail: moto sem motorista")
+            return
+        if self.__passageiro != None:
             print("fail: moto ja tem passageiro")
             return
-        self._passageiro = passageiro
+        self.__passageiro = passageiro
     def descer(self):
-        if self.getPasss().getMoney() < self.getCusto():
+        if self.getPass().getMoney() < self.getCusto():
+            print("fail: Passenger does not have enough money")
+            self.getPass().pagar(self.getCusto())
+            self.getDriver().receber(self.getCusto())
+            self.getPass().setMoney(0)
+            print(f"{self.getPass()} left")
+            self.__passageiro = None
+            self.__custo = 0
+        else:
+            self.getPass().pagar(self.getCusto())
+            self.getDriver().receber(self.getCusto())
+            print(f"{self.getPass()} left")
+            self.__passageiro = None
+            self.__custo = 0
 
-    
+    def dirigir(self, km: int) -> int:
+        if self.__motorista == None:
+            print("fail: moto sem motorista")
+            return
+        if self.__passageiro == None:
+            print("fail: moto sem passageiro")
+            return
+        self.__custo += km
 
-
+    def __str__(self) -> str:
+        return f"Cost: {self.__custo}, Driver: {self.__motorista}, Passenger: {self.__passageiro}"
 
 def main():
     moto = Moto(0)
@@ -66,5 +91,4 @@ def main():
             moto.dirigir(km)
         elif args[0] == "leavePass":
             moto.descer()
-
 main()
